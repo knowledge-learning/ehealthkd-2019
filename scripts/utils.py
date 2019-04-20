ad0a46312e0d92487aa5bf05c09cd37fba66ff17
Juan Pablo Consuegra Ayala <jpconsuegra93@gmail.com>
2019-04-19 20:20:33 -0400

    Add DisjointSet to utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -369,3 +369,59 @@ class Collection:
             s.sort()
 
         self.sentences.extend(sentences_obj)
+
+class DisjointSet:
+    def __init__(self, *items):
+        self.nodes = { x: DisjointNode(x) for x in items }
+
+    def merge(self, items):
+        items = (self.nodes[x] for x in items)
+        try:
+            head, *others = items
+            for other in others:
+                head.merge(other)
+        except ValueError:
+            pass
+
+    @property
+    def representatives(self):
+        return { n.representative for n in self.nodes.values() }
+
+    @property
+    def groups(self):
+        return [[n for n in self.nodes.values() if n.representative == r] for r in self.representatives]
+
+    def __len__(self):
+        return len(self.representatives)
+
+    def __getitem__(self, item):
+        return self.nodes[item]
+
+    def __call__(self, item1, item2):
+        return self[item1].representative == self[item2].representative
+
+    def __str__(self):
+        return str(self.groups)
+
+    def __repr__(self):
+        return str(self)
+
+class DisjointNode:
+    def __init__(self, value):
+        self.value = value
+        self.parent = self
+
+    @property
+    def representative(self):
+        if self.parent != self:
+            self.parent = self.parent.representative
+        return self.parent
+
+    def merge(self, other):
+        other.representative.parent = self.representative
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self)
